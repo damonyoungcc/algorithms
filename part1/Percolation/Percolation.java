59141e10566a3eaaa3431d454073e48bd8a07f68
damonyoungcc <damonyoungcc@icloud.com>
2023-08-26 00:29:07 +0800

    feat: Percolation

--- part1/Percolation/Percolation.java
+++ part1/Percolation/Percolation.java
@@ -13,20 +13,22 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private static int top;
-    private static int bottom;
-    private static int number;
-    private static int[][] state;
-    WeightedQuickUnionUF wquf;
+    private static int top; // top 虚位，当第一行有 site 状态变为 1时，将top 和 它进行 union 操作
+    private static int bottom; // bottom 虚位，当最后一行有 site 状态变为 1时，将top 和 它进行 union 操作
+    private static int number; // 当有 site 状态变为 1时，将 number + 1
+    private static int[][] state; // 初始化一个二维数组 row col 记录每个 site 的状态
+    private static WeightedQuickUnionUF wquf;
+    private static int size; // 记录初始化的 N 的值
 
     // creates n-by-n grid, with all sites initially blocked
     // 初始化一个 n * n 网格，所有的节点都设置为 blocked
     public Percolation(int n) {
-        top = n * n + 1;
-        bottom = n * n + 2;
+        top = n * n;
+        bottom = n * n + 1;
         wquf = new WeightedQuickUnionUF(n * n + 2);
         number = 0;
         state = new int[n][n];
+        size = n;
     }
 
     // opens the site (row, col) if it is not open already
@@ -35,7 +37,38 @@ public class Percolation {
         if (!this.isOpen(row, col)) {
             state[row][col] = 1;
             this.updateNumberOfOpenSites();
+            int currentSiteIndex = getIndexByRowAndCol(row, col);
+            // union
+            if (row == 0) {
+                wquf.union(top, currentSiteIndex);
+                unionSite(currentSiteIndex, row, col);
+            } else if (row == size - 1) {
+                wquf.union(bottom, currentSiteIndex);
+                unionSite(currentSiteIndex, row, col);
+            } else {
+                unionSite(currentSiteIndex, row, col);
+            }
         }
+    }
+
+    private void unionSite(int currentSite, int row, int col) {
+        int topSite = this.getIndexByRowAndCol(row - 1, col);
+        int rightSite = this.getIndexByRowAndCol(row, col + 1);
+        int bottomSite = this.getIndexByRowAndCol(row + 1, col);
+        int leftSite = this.getIndexByRowAndCol(row, col - 1);
+        int[] sites = { topSite, rightSite, bottomSite, leftSite };
+        for (int site : sites) {
+            if (site != -1) {
+                wquf.union(currentSite, site);
+            }
+        }
+    }
+
+    private int getIndexByRowAndCol(int row, int col) {
+        if (row < 0 || col < 0 || row == size || col == size || !this.isOpen(row, col)) {
+            return -1;
+        }
+        return (row) * size + col;
     }
 
     // is the site (row, col) open?
@@ -63,22 +96,19 @@ public class Percolation {
     // does the system percolate?
     // 系统是否是渗透
     public boolean percolates() {
-        return this.wquf.find(top) == this.wquf.find(bottom);
+        return wquf.find(top) == wquf.find(bottom);
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        int N = 10;
-        Percolation pl = new Percolation(10);
+        int N = 4000;
+        Percolation pl = new Percolation(N);
 
         while (!pl.percolates()) {
-            int i = StdRandom.uniformInt(1, N + 1);
-            int j = StdRandom.uniformInt(1, N + 1);
-            System.out.println(i);
-            System.out.println(j);
+            int i = StdRandom.uniformInt(0, N);
+            int j = StdRandom.uniformInt(0, N);
             if (!pl.isOpen(i, j)) {
                 pl.open(i, j);
-                number += 1;
             }
         }
 
--- part1/Percolation/PercolationStats.java
+++ part1/Percolation/PercolationStats.java
@@ -1,14 +1,35 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+
+    private final int t;
+    private final double[] pList;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
 
+        t = trials;
+        pList = new double[t];
+        for (int index = 0; index < trials; index++) {
+            Percolation pl = new Percolation(n);
+            while (!pl.percolates()) {
+                int i = StdRandom.uniformInt(0, n);
+                int j = StdRandom.uniformInt(0, n);
+                if (!pl.isOpen(i, j)) {
+                    pl.open(i, j);
+                }
+            }
+            double p = (double) pl.numberOfOpenSites() / (n * n);
+            StdOut.println(p);
+            pList[index] = p;
+        }
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        return 0;
-
+        return StdStats.mean(pList);
     }
 
     // sample standard deviation of percolation threshold
@@ -30,6 +51,7 @@ public class PercolationStats {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        PercolationStats ps = new PercolationStats(8000, 10);
+        StdOut.println("mean                    = " + ps.mean());
     }
 }
